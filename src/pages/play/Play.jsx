d192c67407236db13376910b6e8b35957923672e
unknown <ozamit7@gmail.com>
2024-12-04 16:32:19 +0100

    adding host

--- src/pages/play/Play.jsx
+++ src/pages/play/Play.jsx
@@ -3,13 +3,15 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import { Reorder } from 'framer-motion';
 
+import { host } from '../../utils/host';
+
 const Play = () => {
     const [drivers, setDrivers] = useState([]); // Store fetched driver data
     const [fetchStatus, setFetchStatus] = useState(''); // Track fetch status
 
     useEffect(() => {
         // Fetch driver data from your backend (which proxies the request to the Ergast API)
-        fetch('http://localhost:5000/api/data') // This hits your backend, not the Ergast API directly
+        fetch(`${host}/api/data`) // This hits your backend, not the Ergast API directly
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
---
+++ src/utils/host.js
@@ -0,0 +1,2 @@
+// export const host = "https://race-vision-be.onrender.com/";
+export const host = "http://localhost:5000";
