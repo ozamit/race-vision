18f04db93c62411234119f7847d39666a8ffacf9
ozamit <your.email@example.comozamit7@gmail.com>
2025-05-08 17:38:49 +0200

    fix host

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,2 +1,2 @@
-// export const host = "https://race-vision-be.onrender.com/";
-export const host = "http://localhost:5001/";
+export const host = "https://race-vision-be.onrender.com/";
+// export const host = "http://localhost:5001/";
